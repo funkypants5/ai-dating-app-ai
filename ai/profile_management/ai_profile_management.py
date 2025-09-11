e6b1bac57002ae9ea355b4d83315a55f38df8351
Ian Tan <iantansw21@gmail.com>
2025-09-11 15:06:47 +0800

    update dependency versions and set langsmith tracing to false

--- ai/profile_management/ai_profile_management.py
+++ ai/profile_management/ai_profile_management.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 def init_ai():
-    os.environ["LANGSMITH_TRACING"] = "true"
+    os.environ["LANGSMITH_TRACING"] = "false"
     os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
     model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
     return model
